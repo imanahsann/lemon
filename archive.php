323600db59c9d02d1bc6053e6960b0e6d77f4c4f
imanahsann <imanahsan@gmail.com>
2020-04-15 09:05:24 -0400

    clean up archive styling

--- archive.php
+++ archive.php
@@ -8,113 +8,92 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<div class="wrapper" id="archive-wrapper">
-
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
-
-		<div class="row">
-
-			<!-- Do the left sidebar check and opens the primary div -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
-
-			<main class="site-main archive-main" id="main">
-
-                <div class="location">
-                    <!-- Logo -->
-                    <div class="logo-container">
-                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="194" viewBox="0 24 194 248">
-                                <g id="Logo" data-name="Logo" transform="translate(-677 -128)">
-                                    <g id="Subheading" transform="translate(740 128)">
-                                    <text id="_9th" data-name="9th" transform="translate(0 97)" fill="#1a1a1a" font-size="90" font-family="PlayfairDisplay-BoldItalic, Playfair Display" font-weight="700" font-style="italic" letter-spacing="-0.06em"><tspan x="0" y="0">9th</tspan></text>
+<main class="site-main archive-main" id="main">
+    <div class="wrapper" id="archive-wrapper">
+	    <div class="container-fluid">
+		    <div class="row">
+			    <div class="col-md">
+                    <div class="row">
+                        <div class="col-md-auto logo-container">
+                            <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="194" viewBox="0 24 194 248">
+                                    <g id="Logo" data-name="Logo" transform="translate(-677 -128)">
+                                        <g id="Subheading" transform="translate(740 128)">
+                                        <text id="_9th" data-name="9th" transform="translate(0 97)" fill="#1a1a1a" font-size="90" font-family="PlayfairDisplay-BoldItalic, Playfair Display" font-weight="700" font-style="italic" letter-spacing="-0.06em"><tspan x="0" y="0">9th</tspan></text>
+                                        </g>
+                                        <text id="_" data-name="&amp;" transform="translate(677 303)" fill="#1a1a1a" font-size="90" font-family="PlayfairDisplay-BoldItalic, Playfair Display" font-weight="700" font-style="italic" letter-spacing="-0.06em"><tspan x="0" y="0">&amp;</tspan></text>
+                                        <text id="Irv_ing" data-name="Irv
+                                    ing" transform="translate(750 303)" fill="#1a1a1a" font-size="90" font-family="PlayfairDisplay-BoldItalic, Playfair Display" font-weight="700" font-style="italic" letter-spacing="-0.06em"><tspan x="0" y="0">I</tspan><tspan y="0" letter-spacing="-0.075em">rv</tspan><tspan x="0" y="74">ing</tspan></text>
                                     </g>
-                                    <text id="_" data-name="&amp;" transform="translate(677 303)" fill="#1a1a1a" font-size="90" font-family="PlayfairDisplay-BoldItalic, Playfair Display" font-weight="700" font-style="italic" letter-spacing="-0.06em"><tspan x="0" y="0">&amp;</tspan></text>
-                                    <text id="Irv_ing" data-name="Irv
-                                ing" transform="translate(750 303)" fill="#1a1a1a" font-size="90" font-family="PlayfairDisplay-BoldItalic, Playfair Display" font-weight="700" font-style="italic" letter-spacing="-0.06em"><tspan x="0" y="0">I</tspan><tspan y="0" letter-spacing="-0.075em">rv</tspan><tspan x="0" y="74">ing</tspan></text>
-                                </g>
-                            </svg>
-                        </a>
-                    </div>
+                                </svg>
+                            </a>
 
-                    <div class="archive-title">
-                        <?php
-						the_archive_title( '<h1>/', '</h1>' );
-						?>
-                    </div>
-
-                </div>
-
-                <div class="archive-content">
-
-					<?php while ( have_posts() ) : the_post(); ?>
-
-						<!-- POST -->
-                        <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-
-                            <div class="blog-thumb-container">
-                                <?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+                            <div class="archive-title">
+                                <?php the_archive_title( '<h1 class="entry-title">/', '</h1>' ); ?>
                             </div>
+                        </div>
+                        <div class="col-md right-content archive-content">
+                            <?php while ( have_posts() ) : the_post(); ?>
 
-                            <header class="entry-header">
+                                <!-- POST -->
+                                <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-                                <?php
-                                the_title(
-                                    sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-                                    '</a></h2>'
-                                );
-                                ?>
+                                    <div class="blog-thumb-container">
+                                        <?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+                                    </div>
 
-                            </header><!-- .entry-header -->
+                                    <header class="entry-header">
 
-                            <div class="entry-content">
+                                        <?php
+                                        the_title(
+                                            sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+                                            '</a></h2>'
+                                        );
+                                        ?>
 
-                                <?php the_excerpt(); ?>
+                                    </header><!-- .entry-header -->
 
-                                <?php
-                                wp_link_pages(
-                                    array(
-                                        'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-                                        'after'  => '</div>',
-                                    )
-                                );
+                                    <div class="entry-content">
 
-                                ?>
+                                        <?php the_excerpt(); ?>
+
+                                        <?php
+                                        wp_link_pages(
+                                            array(
+                                                'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+                                                'after'  => '</div>',
+                                            )
+                                        );
+
+                                        ?>
 
 
-                            </div><!-- .entry-content -->
+                                    </div><!-- .entry-content -->
 
-                            <footer class="entry-footer">
+                                    <footer class="entry-footer">
 
-                            <?php
+                                    <?php
 
-                                $tags_list = get_the_tag_list( '', esc_html__( ' ', 'understrap' ) );
+                                        $tags_list = get_the_tag_list( '', esc_html__( ' ', 'understrap' ) );
 
-                                if ( $tags_list ) {
-                                    /* translators: %s: Tags of current post */
-                                    printf( '<div class="tags-links">' . esc_html__( '%s', 'understrap' ) . '</div>', $tags_list ); // WPCS: XSS OK.
-                                }
+                                        if ( $tags_list ) {
+                                            /* translators: %s: Tags of current post */
+                                            printf( '<div class="tags-links">' . esc_html__( '%s', 'understrap' ) . '</div>', $tags_list ); // WPCS: XSS OK.
+                                        }
 
-                            ?>
+                                    ?>
 
-                            </footer><!-- .entry-footer -->
+                                    </footer><!-- .entry-footer -->
 
-                        </article><!-- #post-## -->
+                                </article><!-- #post-## -->
 
-                    <?php endwhile; ?>
-
+                            <?php endwhile; ?>
+                        </div>
+                    </div>
                 </div>
-
-			</main><!-- #main -->
-
-			<!-- The pagination component -->
-			<?php understrap_pagination(); ?>
-
-		</div><!-- .row -->
-
-	</div><!-- #content -->
-
-</div><!-- wrapper -->
-
-</div>
+            </div><!-- .row -->
+        </div><!-- #content -->
+    </div><!-- wrapper -->
+</main><!-- #main -->
 
 <?php get_footer(); ?>
